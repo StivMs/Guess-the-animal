b88697164bef861c14a0e8215c858139f3ab254b
Stivan Mersho <stivan.ms@outlook.com>
2020-03-22 20:00:08 +0100

    Initial Commit

---
+++ GuessXAnimal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessXAnimal
+//
+//  Created by Stivan Mersho on 2020-03-22.
+//  Copyright © 2020 Stivan Mersho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
